d1253c5f548415209d14002b1769556f2bba2d50
rgaglian <rgaglian@78c3b832-1aed-11de-be07-9135ae5da72a>
2009-05-07 04:58:35 +0000

    Improve server side script in test.pl, still need the POST hack. r.

--- simon-applet/src/simon/XML/database.sql
+++ simon-applet/src/simon/XML/database.sql
@@ -1,59 +1,184 @@
-DROP ROLE simon;
+--
+-- PostgreSQL database dump
+--
 
-CREATE ROLE simon LOGIN
-  ENCRYPTED PASSWORD 'md570df7ab0acf02a3fa119cb4bca37b868'
-  NOSUPERUSER NOINHERIT NOCREATEDB NOCREATEROLE;	
+SET client_encoding = 'UTF8';
+SET check_function_bodies = false;
+SET client_min_messages = warning;
 
-DROP DATABASE simon;
+--
+-- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: simon
+--
 
-CREATE DATABASE simon WITH OWNER simon;
+COMMENT ON SCHEMA public IS 'standard public schema';
 
-DROP TABLE results;
 
-DROP SEQUENCE results_id_seq;
+SET search_path = public, pg_catalog;
 
-CREATE SEQUENCE results_id_seq;
+--
+-- Name: results_id_seq; Type: SEQUENCE; Schema: public; Owner: simon
+--
+
+CREATE SEQUENCE results_id_seq
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.results_id_seq OWNER TO simon;
+
+--
+-- Name: results_id_seq; Type: SEQUENCE SET; Schema: public; Owner: simon
+--
+
+SELECT pg_catalog.setval('results_id_seq', 1, true);
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: results; Type: TABLE; Schema: public; Owner: simon; Tablespace: 
+--
 
 CREATE TABLE results (
-        	id integer unique primary key,
-        	date_test   timestamp,
-        	version    integer,
-        	ip_origin    inet,
-        	ip_destination   inet,
-        	testtype	 varchar(20),
-        	number_probes   integer,
-        	min_rtt	 integer,
-        	max_rtt	 integer,
-        	ave_rtt	 integer,
-        	dev_rtt	 integer,
-        	packet_loss 	integer,
-        	country_origin	  char(2),
-        	country_destination	  char(2)
+    id integer DEFAULT nextval('results_id_seq'::regclass) NOT NULL,
+    date_test timestamp with time zone,
+    version integer,
+    ip_origin inet,
+    ip_destination inet,
+    testtype character varying(20),
+    number_probes integer,
+    min_rtt integer,
+    max_rtt integer,
+    ave_rtt integer,
+    dev_rtt integer,
+    packet_loss integer,
+    country_origin character(2),
+    country_destination character(2)
 );
 
-ALTER TABLE results
-    ALTER COLUMN id 
-        SET DEFAULT NEXTVAL('results_id_seq');
-        
-DROP TABLE test_points;
 
-DROP SEQUENCE testpointid_seq;
-        
-CREATE SEQUENCE testpointid_seq;
+ALTER TABLE public.results OWNER TO simon;
+
+--
+-- Name: testpointid_seq; Type: SEQUENCE; Schema: public; Owner: simon
+--
+
+CREATE SEQUENCE testpointid_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MAXVALUE
+    NO MINVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.testpointid_seq OWNER TO simon;
+
+--
+-- Name: testpointid_seq; Type: SEQUENCE SET; Schema: public; Owner: simon
+--
+
+SELECT pg_catalog.setval('testpointid_seq', 1, false);
+
+
+--
+-- Name: test_points; Type: TABLE; Schema: public; Owner: simon; Tablespace: 
+--
 
 CREATE TABLE test_points (
-      	testpointid integer unique primary key,
-      	description text,
-      	testtype    varchar(20),
-      	ip_address	 inet,
-      	country	  varchar(2),
-      	enabled   boolean,
-      	date_created    timestamp
+    testpointid integer DEFAULT nextval('testpointid_seq'::regclass) NOT NULL,
+    description text,
+    testtype character varying(20),
+    ip_address inet,
+    country character varying(2),
+    enabled boolean,
+    date_created timestamp without time zone
 );
 
-ALTER TABLE test_points
-    ALTER COLUMN testpointid
-        SET DEFAULT NEXTVAL('testpointid_seq');
 
-GRANT ALL PRIVILEGES on results to simon;
-GRANT ALL PRIVILEGES on test_points to simon;
+ALTER TABLE public.test_points OWNER TO simon;
+
+
+--
+-- Data for Name: test_points; Type: TABLE DATA; Schema: public; Owner: simon
+--
+
+COPY test_points (testpointid, description, testtype, ip_address, country, enabled, date_created) FROM stdin;
+\.
+
+
+--
+-- Name: results_pkey; Type: CONSTRAINT; Schema: public; Owner: simon; Tablespace: 
+--
+
+ALTER TABLE ONLY results
+    ADD CONSTRAINT results_pkey PRIMARY KEY (id);
+
+
+ALTER INDEX public.results_pkey OWNER TO simon;
+
+--
+-- Name: test_points_pkey; Type: CONSTRAINT; Schema: public; Owner: simon; Tablespace: 
+--
+
+ALTER TABLE ONLY test_points
+    ADD CONSTRAINT test_points_pkey PRIMARY KEY (testpointid);
+
+
+ALTER INDEX public.test_points_pkey OWNER TO simon;
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: simon
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM simon;
+GRANT ALL ON SCHEMA public TO simon;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- Name: results_id_seq; Type: ACL; Schema: public; Owner: simon
+--
+
+REVOKE ALL ON TABLE results_id_seq FROM PUBLIC;
+REVOKE ALL ON TABLE results_id_seq FROM simon;
+GRANT SELECT,UPDATE ON TABLE results_id_seq TO simon;
+
+
+--
+-- Name: results; Type: ACL; Schema: public; Owner: simon
+--
+
+REVOKE ALL ON TABLE results FROM PUBLIC;
+REVOKE ALL ON TABLE results FROM simon;
+GRANT INSERT,SELECT,UPDATE,DELETE,REFERENCES,TRIGGER ON TABLE results TO simon;
+
+
+--
+-- Name: testpointid_seq; Type: ACL; Schema: public; Owner: simon
+--
+
+REVOKE ALL ON TABLE testpointid_seq FROM PUBLIC;
+REVOKE ALL ON TABLE testpointid_seq FROM simon;
+GRANT SELECT,UPDATE ON TABLE testpointid_seq TO simon;
+
+
+--
+-- Name: test_points; Type: ACL; Schema: public; Owner: simon
+--
+
+REVOKE ALL ON TABLE test_points FROM PUBLIC;
+REVOKE ALL ON TABLE test_points FROM simon;
+GRANT INSERT,SELECT,UPDATE,DELETE,REFERENCES,TRIGGER ON TABLE test_points TO simon;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+
+
